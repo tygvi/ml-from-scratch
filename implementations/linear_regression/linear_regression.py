e36abb9b6617beeb6d2d497038e8f57815d21fba
tygvi <aavarttyagi@gmail.com>
2026-07-24 11:06:53 +0530

    Update linear_regression.py

---
+++ implementations/linear_regression/linear_regression.py
@@ -0,0 +1,12 @@
+class LinearRegression:
+  def __init__(self,learning_rate=0.01, epochs=1000):
+    pass
+  def fit(self,X,y):
+    pass
+  def predict(self,X,y):
+    pass
+  def _compute_cost(self,X,y):
+    pass
+  def _gradient_descent(self,X,y):
+    pass
+    
