4fa2cd4ac862fa971519dcc1b2a1b7f96306eeba
Aavart Tyagi <aavarttyagi@gmail.com>
2026-07-24 11:51:12 +0530

    Linear Regression

--- implementations/linear_regression/linear_regression.py
+++ implementations/linear_regression/linear_regression.py
@@ -1,12 +1,36 @@
 class LinearRegression:
   def __init__(self,learning_rate=0.01, epochs=1000):
-    pass
+  	self.w = 0
+    self.b = 0
+    self.learning_rate = learning_rate
+    self.epochs = epochs
+
+
   def fit(self,X,y):
-    pass
-  def predict(self,X,y):
-    pass
+  	 X = np.asarray(X)
+     y = np.asarray(y)
+     self._gradient_descent(X, y)
+     return self
+
+  def predict(self,X):
+  	return self.w*X+self.b
+
   def _compute_cost(self,X,y):
-    pass
+  	y_pred=self.w*x+self.b
+  	cost=(1/(2*len(X)))*np.sum((y_pred-y)**2)
+  	return cost 
+  
   def _gradient_descent(self,X,y):
-    pass
-    
+  	m=len(X)
+  	for _ in range(self.epochs):
+  		dj_dw=0
+  		dj_db=0
+  		for g in range(m):
+  			prediction=self.predict(X[g])
+  			error=prediction-y[g]
+  			dj_dw+=error*X[g]
+  			dj_db+=error
+  		dj_dw=dj_dw/m
+  		dj_db=dj_db/m
+  		self.w=self.w-self.learning_rate*dj_dw
+  		self.b=self.b-self.learning_rate*dj_db
